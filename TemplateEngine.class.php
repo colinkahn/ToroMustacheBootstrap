98f5e3d6902e999d3fc323726481b87a0e8f5a4f
Colin Kahn <colin@magneticcreative.com>
2012-04-08 22:51:54 -0700

    adding stuff, wysihtml5 and stuff

---
+++ TemplateEngine.class.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'inc/Mustache.php';
+require_once 'inc/MustacheLoader.php';
+
+class TemplateEngine extends Mustache { 
+    public $basedir = "/~temp/toro/";
+ 
+    public $navigation = array(
+        array('name'=>'Home', 'url'=>''),
+        array('name'=>'Test Something', 'url'=>'test/something'),
+        array('name'=>'Test Nothing', 'url'=>'test/nothing'),
+        array('name'=>'Form', 'url'=>'form'),
+        array('name'=>'WYSIWYG', 'url'=>'wysiwyg'),
+    );
+     
+    public function __construct($template = null, $view = null, $partials = null, array $options = null) {
+        if ( is_null($template) )
+            $template = "{{> header }}{{> navigation }}{{> content }}{{> footer }}";
+            
+        if ( is_null($partials) )
+            $partials = new MustacheLoader(dirname(__FILE__).'/fixtures');
+        
+        parent::__construct($template, $view, $partials, $options);
+    }
+    
+    public function addToContext($data = array()) {
+        foreach ($data as $key => $value) {
+            $this->{$key} = $value;
+        }
+    }
+   
+}
--- index.php
+++ index.php
@@ -1,39 +1,82 @@
 <?php
 require_once 'toro.php';
-require_once 'inc/Mustache.php';
-require_once 'inc/MustacheLoader.php';
+require_once 'TemplateEngine.class.php';
 
-class ToroMustache extends Mustache { 
-    public $basedir = "/~temp/toro/";
-        
-    public function __construct() {
-        $this->_template = "{{> header }}{{> navigation }}{{> content}}{{> footer }}";
-        $this->_partials = new MustacheLoader(dirname(__FILE__).'/fixtures');
-        parent::__construct();
-    }
-   
-}
 
 class MainHandler extends ToroHandler {
     public function get() {
-        $m = new ToroMustache();
-        $m->content = '<h1>Bootstrap starter template</h1><p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>';
-		echo $m->render();
+        $t = new TemplateEngine();
+        $t->content = '<h1>Bootstrap starter template</h1><p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>';
+		echo $t->render();
     }
 }
+
 
 class TestHandler extends ToroHandler {
-    public function get() {
-        $m = new ToroMustache();
-        $m->content = "<h1>This is a test</h1>";
-		echo $m->render();
+    public $template = "{{> header }}{{> navigation }}{{> test }}{{> footer }}";
+    
+    public function get( $testing ) {
+        $t = new TemplateEngine($this->template);
+        $t->content = $testing;
+		echo $t->render();
     }
 }
 
+class FormHandler extends ToroHandler {
+    public $template = "{{> header }}{{> navigation }}{{> form }}{{> footer }}";
+    public $context = array();
+    
+    public function get() {
+       $t = new TemplateEngine($this->template);
+       $t->addToContext($this->context);
+       echo $t->render(); 
+    }
+    
+    public function post() {
+        $this->context = array(
+            'something' => $_POST['something'],
+            'lookhere'  => isset($_POST['lookhere']) ? $_POST['lookhere'] : null,
+            'alert'     => ($_POST['something'] && isset($_POST['lookhere'])) ? array('success'=>true) : array('fail'=>true)
+        );
+        
+        if ( !isset($this->context['alert']['success']) ) {
+            if ( !($_POST['something'] || isset($_POST['lookhere'])) ) {
+                $this->context['alert']['none'] = true;
+            } else {
+                $this->context['alert']['partial'] = true;
+            }
+        }
+        
+        $this->get();
+    }
+}
+
+class WysiwygHandler extends ToroHandler {
+    public $template = "{{> header }}{{> navigation }}{{> wysiwyg }}{{> footer }}";
+    public $context = array();
+
+    public function get() {
+        $t = new TemplateEngine($this->template);
+        $t->extra_foot = '<script type="text/javascript">$("#textarea").wysihtml5();</script>';
+        $t->addToContext($this->context);
+        echo $t->render();
+    }
+    
+    public function post() {
+        $this->context = array(
+            'status'=>array('posted'=>true),
+            'results'=>$_POST['textarea']
+        );
+        $this->get();
+    }
+}
 
 $site = new ToroApplication(array(
     array('/', 'MainHandler'),
-    array('test/something', 'TestHandler'),
-));
+    array('test/([a-z]+)', 'TestHandler'),
+    array('form', 'FormHandler'),
+    array('wysiwyg', 'WysiwygHandler'),
+));    
+
 
 $site->serve();
